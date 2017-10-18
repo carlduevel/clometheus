a584c14d1409286ac97820fb9d846255d61a385f
Carl Düvel <c.a.duevel@gmail.com>
2017-10-18 18:27:35 +0200

    project.clj cleanup

--- project.clj
+++ project.clj
@@ -1,10 +1,5 @@
 (defproject online.duevel/clometheus "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Prometheus client library"
   :license {:name "Eclipse Public License"
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
-  :java-source-paths ["src/java"]
-  :source-paths ["src/clojure"]
-  :dependencies [[org.clojure/clojure "1.8.0"]
-                 [io.prometheus/simpleclient "0.0.26"]]
-  )
+  :dependencies [[org.clojure/clojure "1.8.0"]])
