780e7cb46247f000f6b29a60eff3719b9ffdf27c
Carl Düvel <c.a.duevel@gmail.com>
2017-10-18 18:23:12 +0200

    Basic structure + MUST requirements for counters and gauges

---
+++ src/clojure/clometheus/core.clj
@@ -0,0 +1,131 @@
+(ns clometheus.core
+  (:import (java.util.concurrent ConcurrentHashMap)
+           (java.util.concurrent.atomic DoubleAdder)
+           (clojure.lang ILookup Keyword IDeref)))
+
+(defprotocol ICollectorRegistry
+  (fetch [this name])
+  (register-or-return! [this collector])
+  (clear! [this])
+  (collect [this]))
+
+
+(defprotocol ICollector
+  (name [this])
+  (description [this])
+  (metric-type [this])
+  (samples [this]))
+
+(deftype CollectorRegistry [^ConcurrentHashMap names->collectors]
+  ICollectorRegistry
+  (fetch [_this name]
+    (.get names->collectors name))                          ; type needs to be checked
+  (register-or-return! [_this collector]
+    (let [name (.name collector)]
+      (if-let [found-collector (.get names->collectors name)]
+        found-collector
+        (or (.putIfAbsent names->collectors (.name collector) collector) collector))))
+  (clear! [_this] (.clear names->collectors))
+  (collect [_this]
+    (flatten (map samples (.values names->collectors)))))
+
+
+(def default-registry (->CollectorRegistry (ConcurrentHashMap.)))
+
+(defrecord Sample [^String name ^String description ^Keyword type label->values ^Double value])
+
+(defmulti new-metric
+          (fn [type] type))
+
+
+
+(deftype Collector [^String name ^String description ^Keyword type ^ConcurrentHashMap label-values->collectors labels]
+  ICollector
+  (name [_this] name)
+  (description [_this] description)
+  (metric-type [this] type)
+  (samples [_this]
+    (for [[label-value collector] label-values->collectors]
+      (->Sample name description type label-value @collector)))
+  ILookup
+  (valAt [this key]
+    (if-let [found-metric (.get label-values->collectors key)]
+      found-metric
+      (let [new-metric         (new-metric type)
+            current-val-or-nil (.putIfAbsent label-values->collectors key new-metric)]
+        (or current-val-or-nil new-metric))))
+  (valAt [this key notfound]
+    (throw (UnsupportedOperationException. "Sorry"))))
+
+;alter-meta to make it private
+; must always be the same set of metrics - exception when one is not set?
+
+
+(defn register-collector [name description labels type]
+  (if-let [collector (fetch default-registry name)]
+    collector
+    (register-or-return! default-registry (->Collector name description type (ConcurrentHashMap.) labels))))
+
+(defprotocol Incrementable
+  (inc! [this] [this ^Double value]))
+
+(defrecord Counter [^DoubleAdder current-val]
+  IDeref
+  (deref [_this] (.sum current-val))
+  Incrementable
+  (inc! [this]
+    (inc! this 1))
+  (inc! [_this increment]
+    (if (pos? increment)
+      (.add current-val increment)
+      (throw (IllegalArgumentException. "Counters cannot be incremented with negative values")))))
+
+(defn counter
+  ([name description]
+   (let [collector (counter name description [])]
+     (get collector {})))
+  ([name description labels]
+   (register-collector name description labels :counter)))
+
+
+
+(defmethod new-metric :counter
+  [_type]
+  (Counter. (DoubleAdder.)))
+
+
+(defprotocol Decrementable
+  (dec! [this] [this value]))
+
+(defprotocol Resettable
+  (set! [this value]))
+
+(defrecord Gauge [^DoubleAdder current-val]
+  IDeref
+  (deref [_this] (.sum current-val))
+  Incrementable
+  (inc! [this] (inc! this 1))
+  (inc! [_this val] (.add current-val val))
+  Decrementable
+  (dec! [this] (dec! this 1))
+  (dec! [_this val] (.add current-val (* -1 val)))
+  Resettable
+  (set! [_this current-val] (locking current-val (.reset current-val) (.add current-val current-val))))
+
+(defmethod new-metric :gauge [_type]
+  (Gauge. (DoubleAdder.)))
+
+(defn gauge
+  ([name description]
+   (let [collector (counter name description [])]
+     (get collector {})))
+  ([name description labels]
+   (register-collector name description labels :gauge)))
+
+
+;
+;(defprotocol Histogram
+;  (observe! [this ^Double value]))
+
+
+
---
+++ test/clometheus/core_test.clj
@@ -0,0 +1,58 @@
+(ns clometheus.core-test
+  (:require [clojure.test :refer :all]
+            [clometheus.core :refer :all]))
+
+(deftest counters-test
+  (clear! default-registry)
+  (let [my-counter (counter "my-counter" "this is my counter")]
+    (testing "counter start at zero"
+      (is (= 0.0 @my-counter)))
+    (testing "incrementing by one works"
+      (inc! my-counter)
+      (is (= 1.0 @my-counter)))
+    (testing "incrementing by more than one works"
+      (inc! (counter "my-counter" "hello") 2)
+      (is (= 3.0 @my-counter)))
+    (testing "counters my only go up"
+      (is (thrown? IllegalArgumentException (inc! my-counter -1))))
+    (testing "already registered counters are returned and not new created"
+      (is (= my-counter (counter "my-counter" "hello"))))
+    (testing "labels are possible"
+      (let [my-label-counter (counter "with-labels" "a counter with labels" ["my-label"])]
+        (inc! (get my-label-counter {:my-label :my-val}))))
+    (testing "counters are collectable"
+      (is (= '(#clometheus.core.Sample{:name "my-counter", :description "this is my counter", :type :counter, :label->values {}, :value 3.0}
+               #clometheus.core.Sample{:name "with-labels", :description "a counter with labels", :type :counter, :label->values {:my-label :my-val}, :value 1.0}) (collect default-registry))))))
+
+(deftest gauge-test
+  (clear! default-registry)
+
+  (let [my-gauge (gauge "labelless-gauge" "Gauge without labels")]
+    (testing "gauges start at zero"
+      (is (= 0.0 @my-gauge)))
+    (testing "incrementing by one works"
+      (inc! my-gauge)
+      (is (= 1.0 @my-gauge)))
+    (testing "incrementing by more than one works"
+      (inc! my-gauge 2)
+      (is (= 3.0 @my-gauge)))
+    (testing "already registered gauges are returned and not new created"
+      (is (= my-gauge (gauge "labelless-gauge" "hello"))))
+    (testing "labels are possible"
+      (let [my-label-gauge (gauge "with-labels" "a gauge with labels" ["my-label"])]
+        (inc! (get my-label-gauge {:my-label :my-val}))))
+    (testing "gauges are collectable"
+      (is (= '(#clometheus.core.Sample{:description   "a gauge with labels"
+                                       :label->values {:my-label :my-val}
+                                       :name          "with-labels"
+                                       :type          :gauge
+                                       :value         1.0}
+                #clometheus.core.Sample{:description   "Gauge without labels"
+                                        :label->values {}
+                                        :name          "labelless-gauge"
+                                        :type          :counter
+                                        :value         3.0}) (collect default-registry))))))
+
+
+
+
