5ac4052dce877199b5b218af2d8c1d6f086112a5
Carl Düvel <c.a.duevel@gmail.com>
2017-11-17 22:36:09 +0100

    Reworked structure

--- src/clometheus/core.clj
+++ src/clometheus/core.clj
@@ -1,7 +1,7 @@
 (ns clometheus.core
   (:import (java.util.concurrent ConcurrentHashMap)
            (java.util.concurrent.atomic DoubleAdder)
-           (clojure.lang ILookup Keyword IDeref)
+           (clojure.lang ILookup Keyword IDeref ExceptionInfo)
            (java.io Writer)))
 
 (defprotocol ICollectorRegistry
@@ -15,7 +15,7 @@
   (name [this])
   (description [this])
   (metric-type [this])
-  (samples [this]))
+  (sample [this]))
 
 (extend-type ConcurrentHashMap
   ICollectorRegistry
@@ -28,21 +28,20 @@
         (or (.putIfAbsent this (.name collector) collector) collector))))
   (clear! [this] (.clear this))
   (collect [this]
-    (flatten (map samples (.values this)))))
+    (map sample (.values this))))
 
 
 (def default-registry (ConcurrentHashMap.))
 
-(defrecord Sample [^String name ^String description ^Keyword type label->values ^Double value])
+(defrecord Sample [^String name ^String description ^Keyword type label->values])
 
 (deftype Collector [^String name ^String description ^Keyword type ^ConcurrentHashMap label-values->collectors labels metric-fn]
   ICollector
   (name [_this] name)
   (description [_this] description)
   (metric-type [this] type)
-  (samples [_this]
-    (for [[label-value collector] label-values->collectors]
-      (->Sample name description type label-value @collector)))
+  (sample [_this]
+    (->Sample name description type (reduce-kv (fn [m k collector] (assoc m k @collector)) {} (into {} label-values->collectors))))
   ILookup
   (valAt [this key]
     (if-let [found-metric (.get label-values->collectors key)]
@@ -63,15 +62,16 @@
     (register-or-return! default-registry (->Collector name description type (ConcurrentHashMap.) labels metric-fn))))
 
 (defprotocol Incrementable
-  (inc! [this] [this ^Double value]))
+  (increment! [this] [this ^Double value]))
+
 
 (defrecord Counter [^DoubleAdder current-val]
   IDeref
   (deref [_this] (.sum current-val))
   Incrementable
-  (inc! [this]
-    (inc! this 1))
-  (inc! [_this increment]
+  (increment! [this]
+    (increment! this 1))
+  (increment! [_this increment]
     (if (pos? increment)
       (.add current-val increment)
       (throw (IllegalArgumentException. "Counters cannot be incremented with negative values")))))
@@ -84,7 +84,7 @@
    (fetch-or-create-collector! name description labels :counter #(Counter. (DoubleAdder.)))))
 
 (defprotocol Decrementable
-  (dec! [this] [this value]))
+  (decrement! [this] [this value]))
 
 (defprotocol Resettable
   (set! [this value]))
@@ -93,11 +93,11 @@
   IDeref
   (deref [_this] (.sum current-val))
   Incrementable
-  (inc! [this] (inc! this 1))
-  (inc! [_this val] (.add current-val val))
+  (increment! [this] (increment! this 1))
+  (increment! [_this val] (.add current-val val))
   Decrementable
-  (dec! [this] (dec! this 1))
-  (dec! [_this val] (.add current-val (* -1 val)))
+  (decrement! [this] (decrement! this 1))
+  (decrement! [_this val] (.add current-val (* -1 val)))
   Resettable
   (set! [_this current-val] (locking current-val (.reset current-val) (.add current-val current-val))))
 
@@ -109,15 +109,96 @@
    (fetch-or-create-collector! name description labels :gauge #(Gauge. (DoubleAdder.)))))
 
 (defprotocol Observable
-  (observe! [this value]))
+  (observation! [this value]))
+
+(defn type-dispatch-one-or-more-args
+  ([this]
+   (type this))
+  ([this & opts]
+   (type this)))
+
+(defn type-dispatch-2-or-more-args
+  ([this val]
+   (type this))
+  ([this val & opts]
+   (type this)))
+
+(defmulti inc! #'type-dispatch-one-or-more-args)
+(defmulti dec! #'type-dispatch-one-or-more-args)
+(defmulti observe! #'type-dispatch-2-or-more-args)
+
+(defmethod inc!
+  Counter
+  ([this]
+    (increment! this))
+  ([this & {:keys [with-labels by] :or {:with-labels {} :by 1}}]
+    (if (empty? with-labels)
+      (increment! this by)
+      (throw (Exception. "No labels are possible for simple counter!")))))
+
+(defmethod inc!
+  Gauge
+  ([this]
+    (increment! this))
+  ([this & {:keys [with-labels by] :or {:with-labels {} :by 1}}]
+    (if (empty? with-labels)
+      (increment! this by)
+      (throw (Exception. "No labels are possible for simple gauge!")))))
+
+(defmethod dec!
+  Gauge
+  ([this]
+    (decrement! this))
+  ([this & {:keys [with-labels by] :or {:with-labels {} :by 1}}]
+    (if (empty? with-labels)
+      (decrement! this by)
+      (throw (Exception. "No labels are possible for simple gauge!")))))
+
+(defmethod inc!
+  Collector
+  ([this]
+    (throw (Exception. "Labels are missing!")))
+  ([this & {:keys [with-labels by] :or {:with-labels {} :by 1}}]
+    (if (empty? with-labels)
+      (throw (Exception. "Labels are missing!"))
+      (-> (get this with-labels) (increment! (or by 1))))))
+
+(defmethod dec!
+  Collector
+  ([this]
+    (throw (Exception. "Labels are missing!")))
+  ([this & {:keys [with-labels by] :or {:with-labels {} :by 1}}]
+    (if (empty? with-labels)
+      (throw (Exception. "Labels are missing!"))
+      (-> (get this with-labels) (decrement! (or by 1))))))
+
+
+
+(defmethod observe!
+  Collector
+  ([this val]
+    (throw (Exception. "Labels are missing!")))
+  ([this val & {:keys [with-labels by] :or {:with-labels {}}}]
+    (if (empty? with-labels)
+      (throw (Exception. "Labels are missing!"))
+      (-> (get this with-labels) (observation! val)))))
 
 (defrecord Histogram [bucket-sizes bucket-adders cumulative-counts]
   IDeref
   (deref [_this] (map #(.sum %) bucket-adders))
   Observable
-  (observe! [this value]
+  (observation! [this value]
     (doseq [[size bucket] (map list bucket-sizes bucket-adders)] (when (<= value size) (.add bucket 1)))
     (.add cumulative-counts 1)))
+
+(defmethod observe!
+  Histogram
+  ([this value] (observation! this value))
+  ([this value & {:keys [with-labels] :or {:with-labels {}}}]
+    (if (empty? with-labels)
+      (observation! this value)
+      (throw (Exception. "No labels are possible for simple histogram!")))))
+
 
 ;TODO: Default for description
 
@@ -125,21 +206,15 @@
 (defn- create-histogram! [buckets]
   (->Histogram buckets (for [i (range (count buckets))] (DoubleAdder.)) (DoubleAdder.)))
 
-(defn histogram [name & {description :descrition buckets :buckets labels :labels :or {:buckets [0.005, 0.01, 0.025, 0.05, 0.075, 0.1, 0.25, 0.5, 0.75, 1, 2.5, 5, 7.5, 10] :labels [] :description ""}}]
+(defn histogram [name & {description :description buckets :buckets labels :with-labels :or {:buckets [0.005, 0.01, 0.025, 0.05, 0.075, 0.1, 0.25, 0.5, 0.75, 1, 2.5, 5, 7.5, 10] :with-labels [] :description ""}}]
   (let [collector (fetch-or-create-collector! name description labels :histogram (partial create-histogram! buckets))]
     (if (empty? labels)
       (get collector {})
       collector)))
 
 
-
 (defmethod print-method Histogram [h ^Writer writer]
   (.write writer (str "Histogram named" (.-bucket_sizes h))))
-
-
-;
-;(defprotocol Histogram
-;  (observe! [this ^Double value]))
 
 
 
--- test/clometheus/core_test.clj
+++ test/clometheus/core_test.clj
@@ -1,8 +1,9 @@
 (ns clometheus.core-test
   (:require [clojure.test :refer :all]
-            [clometheus.core :refer :all]))
+            [clometheus.core :refer :all])
+  (:import (clometheus.core Counter Collector)))
 
-(deftest counters-test
+(deftest counters-wo-labels-test
   (clear! default-registry)
   (let [my-counter (counter "my-counter" "this is my counter")]
     (testing "counter start at zero"
@@ -11,22 +12,41 @@
       (inc! my-counter)
       (is (= 1.0 @my-counter)))
     (testing "incrementing by more than one works"
-      (inc! (counter "my-counter" "hello") 2)
+      (inc! (counter "my-counter" "hello") :by 2)
       (is (= 3.0 @my-counter)))
-    (testing "counters my only go up"
-      (is (thrown? IllegalArgumentException (inc! my-counter -1))))
+    (testing "counters may only go up"
+      (is (thrown? IllegalArgumentException (inc! my-counter :by -1))))
     (testing "already registered counters are returned and not new created"
       (is (= my-counter (counter "my-counter" "hello"))))
-    (testing "labels are possible"
-      (let [my-label-counter (counter "with-labels" "a counter with labels" ["my-label"])]
-        (inc! (get my-label-counter {:my-label :my-val}))))
     (testing "counters are collectable"
-      (is (= '(#clometheus.core.Sample{:name "my-counter", :description "this is my counter", :type :counter, :label->values {}, :value 3.0}
-               #clometheus.core.Sample{:name "with-labels", :description "a counter with labels", :type :counter, :label->values {:my-label :my-val}, :value 1.0}) (collect default-registry))))))
+      (is (= '(#clometheus.core.Sample{:name "my-counter" :description "this is my counter" :type :counter :label->values {{} 3.0}})
+             (collect default-registry))))))
+
+(deftest counters-with-labels-test
+  (clear! default-registry)
+  (let [my-counter (counter "my-counter" "blabla" ["rc"])]
+    (testing "Labels are managed by Collectors"
+      (is (= Collector (type my-counter))))
+    (testing "Incrementing by more than one works"
+      (inc! my-counter :by 2 :with-labels {:rc 500})
+      (is (== 2 @(get my-counter {:rc 500}))))
+    (testing "Incrementing by just one works"
+      (inc! my-counter :with-labels {:rc 500})
+      (is (== 3 @(get my-counter {:rc 500}))))
+    (testing "Counters may only go up"
+      (is (thrown? IllegalArgumentException
+                   (inc! my-counter :with-labels {:rc 500} :by -1))))
+    (testing "Each label combination is counted by itself"
+      (inc! my-counter :with-labels {:rc 200})
+      (is (== 1 @(get my-counter {:rc 200}))))
+    (testing "counters are collectable"
+      (is (= '(#clometheus.core.Sample{:name "my-counter" :description "blabla" :type :counter :label->values {{:rc 200} 1.0 {:rc 500} 3.0}})
+             (collect default-registry))))))
+
+
 
 (deftest gauge-test
   (clear! default-registry)
-
   (let [my-gauge (gauge "labelless-gauge" "Gauge without labels")]
     (testing "gauges start at zero"
       (is (= 0.0 @my-gauge)))
@@ -34,28 +54,26 @@
       (inc! my-gauge)
       (is (= 1.0 @my-gauge)))
     (testing "incrementing by more than one works"
-      (inc! my-gauge 2)
+      (inc! my-gauge :by 2)
       (is (= 3.0 @my-gauge)))
     (testing "already registered gauges are returned and not new created"
       (is (= my-gauge (gauge "labelless-gauge" "hello"))))
     (testing "labels are possible"
       (let [my-label-gauge (gauge "with-labels" "a gauge with labels" ["my-label"])]
-        (inc! (get my-label-gauge {:my-label :my-val}))))
+        (inc! my-label-gauge :with-labels {:my-label :my-val})))
     (testing "gauges are collectable"
       (is (= '(#clometheus.core.Sample{:description   "a gauge with labels"
-                                       :label->values {:my-label :my-val}
+                                       :label->values {{:my-label :my-val} 1.0}
                                        :name          "with-labels"
-                                       :type          :gauge
-                                       :value         1.0}
+                                       :type          :gauge}
                 #clometheus.core.Sample{:description   "Gauge without labels"
-                                        :label->values {}
+                                        :label->values {{} 3.0}
                                         :name          "labelless-gauge"
-                                        :type          :gauge
-                                        :value         3.0}) (collect default-registry))))))
+                                        :type          :gauge}) (collect default-registry))))))
 
-(deftest histogram-test
+(deftest labeless-histogram-test
   (clear! default-registry)
-  (let [my-histogram (histogram "my-histogram" :description "labeless histogram" :buckets [0.1 1 10] )]
+  (let [my-histogram (histogram "my-histogram" :description "labeless histogram" :buckets [0.1 1 10])]
     (testing "histogram exists"
       (is (not= nil my-histogram)))
     (testing "histograms start with all buckets set to zero"
@@ -64,8 +82,22 @@
       (observe! my-histogram 2)
       (is (= [0.0 0.0 1.0] @my-histogram)))
     (testing "already registered histograms are returned and not new created"
-      (is (= my-histogram (histogram "my-histogram" "labeless histogram" [0.1 1 10]))))))
+      (is (= my-histogram (histogram "my-histogram" "labeless histogram" [0.1 1 10]))))
+    (testing "histograms are collectable"
+      (is (= '(#clometheus.core.Sample{:name "my-histogram", :description nil, :type :histogram, :label->values {{} (0.0 0.0 1.0)}})) (collect default-registry)))))
 
+(deftest histogram-with-test
+  (clear! default-registry)
+  (let [my-histogram (histogram "histogram-with-labels" :description "histogram with labels" :buckets [0.1 1 10] :with-labels ["test"])]
+    (testing "histogram exists"
+      (is (not= nil my-histogram)))
+    (testing "histograms start with all buckets set to zero"
+      (is (= [0.0 0.0 0.0] @(get my-histogram {:test :test}))))
+    (testing "histograms can observe values"
+      (observe! my-histogram 2 :with-labels {:test :best}))
+    (testing "already registered histograms are returned and not new created"
+      (is (= my-histogram (histogram "histogram-with-labels" :description "histogram with labels" :buckets [0.1 1 10] :with-labels ["test"]))))
+    (testing "histograms are collectable"
+      (is (= (list (->Sample "histogram-with-labels" "histogram with labels" :histogram {{:test :best} '(0.0 0.0 1.0) {:test :test} '(0.0 0.0 0.0)})) (collect default-registry)))))
 
-
-
+  )
