6999c18d090714e12120f727d2081682363072a1
Carl Düvel <c.a.duevel@gmail.com>
2018-06-14 20:58:39 +0200

    Version 0.1.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject online.duevel/clometheus "0.1.0-SNAPSHOT"
+(defproject online.duevel/clometheus "0.1.0"
   :description "Prometheus client library"
   :license {:name "Apache License 2.0"
             :url  "https://www.apache.org/licenses/LICENSE-2.0"}
