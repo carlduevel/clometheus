a2a4b9e64e09aab674946499dfb26aee85b71160
Carl Düvel <c.a.duevel@gmail.com>
2018-06-14 21:52:19 +0200

    lein-release plugin: bumped version from 0.1.0 to 0.1.1-SNAPSHOT for next development cycle

--- project.clj
+++ project.clj
@@ -1,6 +1,9 @@
-(defproject online.duevel/clometheus "0.1.0"
+(defproject online.duevel/clometheus "0.1.1-SNAPSHOT"
   :description "Prometheus client library"
+  :url "https://github.com/hackbert/clometheus"
   :license {:name "Apache License 2.0"
             :url  "https://www.apache.org/licenses/LICENSE-2.0"}
+  :lein-release {:deploy-via :clojars}
   :java-source-paths ["java"]
-  :dependencies [[org.clojure/clojure "1.9.0"]])
+  :dependencies [[org.clojure/clojure "1.9.0"]]
+  :profiles {:dev {:plugins [[lein-release/lein-release "1.0.9"]]}})
