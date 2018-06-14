1c843892bbddaa55bfde2a2a5daec6a73e57f07b
Carl Düvel <c.a.duevel@gmail.com>
2018-06-14 20:28:01 +0200

    correct license in project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject online.duevel/clometheus "0.1.0-SNAPSHOT"
   :description "Prometheus client library"
-  :license {:name "Eclipse Public License"
-            :url  "http://www.eclipse.org/legal/epl-v10.html"}
+  :license {:name "Apache License 2.0"
+            :url  "https://www.apache.org/licenses/LICENSE-2.0"}
   :java-source-paths ["java"]
   :dependencies [[org.clojure/clojure "1.9.0"]])
