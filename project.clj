8fe8eb40e5c215cbc06bd597bfe1575c67c4ec1e
Carl Düvel <c.a.duevel@gmail.com>
2021-04-02 16:38:47 +0200

    Ensure java 6 compatibility

--- project.clj
+++ project.clj
@@ -3,7 +3,9 @@
   :url "https://github.com/carlduevel/clometheus"
   :license {:name "Apache License 2.0"
             :url  "https://www.apache.org/licenses/LICENSE-2.0"}
+  :min-lein-version "2.0.0"
   :lein-release {:deploy-via :clojars}
   :java-source-paths ["java"]
+  :javac-options     ["-target" "1.6" "-source" "1.6"]
   :dependencies [[org.clojure/clojure "1.10.3"]]
   :profiles {:dev {:plugins [[lein-release/lein-release "1.0.9"]]}})
